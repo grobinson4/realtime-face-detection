34155a70e6b284270d2310b6e906f7b204f11d25
Gary D. Robinson <gary.robinson2524@gmail.com>
2020-10-22 21:50:50 -0400

    Added age and gender recognition model

--- faceDetection.js
+++ faceDetection.js
@@ -1,10 +1,12 @@
 const video = document.getElementById('video')
+const age = document.getElementById('age')
 
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('./models')
+    faceapi.nets.faceExpressionNet.loadFromUri('./models'),
+    faceapi.nets.ageGenderNet.loadFromUri('./models')
 ]).then(startVideo())
 
 function startVideo() {
@@ -26,12 +28,17 @@ video.addEventListener('play', () => {
     faceapi.matchDimensions(canvas, displaySize)
     setInterval(async () => {
         const detections = await faceapi.detectAllFaces(video, 
-            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender()
             const resizeDetections = faceapi.resizeResults(detections, displaySize)
             canvas.getContext('2d').clearRect(0,0, canvas.width, canvas.height)
             faceapi.draw.drawDetections(canvas, resizeDetections)
             faceapi.draw.drawFaceLandmarks(canvas, resizeDetections)
             faceapi.draw.drawFaceExpressions(canvas, resizeDetections)
+            resizeDetections.forEach( detection => {
+                const box = detection.detection.box
+                const drawBox = new faceapi.draw.DrawBox(box, { label: Math.round(detection.age) + " year old " + detection.gender })
+                drawBox.draw(canvas)
+              })
     }, 100)
 })
 
